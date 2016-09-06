eeb35741957cc1b81f116d02ca74dd7ad5c5dd78
om139 <omprakash1993@outlook.com>
2016-09-06 23:45:54 +0530

    Creating Directive
    Directive for my profile

--- Script/app.js
+++ Script/app.js
@@ -34,3 +34,5 @@ app.config(function ($routeProvider) {
         });
 });
 var app1 = angular.module('innerDesignApp', []);
+
+
--- Script/controller.js
+++ Script/controller.js
@@ -29,8 +29,27 @@ app.controller('innerDesignController',function ($scope) {
 });
 app1.controller('innerDesignController',function ($scope) {
     $scope.empInformation = [];
-    $scope.empInformation.push({Name : 'Rahul', email : 'rahul123@gmail.com', pwd : 'rahul123' , gender:'Male' , dob : '1/1/1990' , doj:'10/8/2016' ,pic:'anguarProject/Design/images1/1.jpg'});
+    $scope.empInformation.push({Name : 'Rahul', email : 'rahul123@gmail.com', pwd : 'rahul123' , gender:'Male' , dob : '1/1/1990' , doj:'10/8/2016' ,pic:'anguarProject/Design/images1/1.jpg' ,permanentAddress:'UP' , correspondanceAddress:'Delhi'});
 });
 app.controller('regpart1Controller',function ($scope) {
 
+});
+
+
+app1.directive('empDetail', function() {
+    return {
+        restrict:E,
+        template: 'Name: {{empInformation.Name}} email: {{empInformation.email} pwd:{{empInformation.pwd}} ' +
+        'gender:{{empInformation.gender}} dob:{{empInformation.dob}} permanentAddress:{{empInformation.permanentAddress}} correspondanceAddress:{{correspondanceAddress.dob}}',
+        templateUrl: 'angularProject/partials/afterRegistration/innerDesign.html',
+        replace: true,
+        transclude: true,
+    };
+
+});
+
+app1.directive('empPicture',function () {
+    return{
+      template:  'pic:{{empInformation.pic}}'
+    };
 });
